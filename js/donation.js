a044a38582947a20532fc4149440754a4ca1dc07
Rizbi ahmmad <contact.rizbi123@gmail.com>
2024-09-25 11:51:07 +0600

    Adding blog section

--- js/donation.js
+++ js/donation.js
@@ -5,7 +5,7 @@ document.getElementById('btn-donate-noakhali')
 
 const donateMoney = getInputFieldValueById('input-donate-noakhali');
 
-if(donateMoney > 0){
+if(donateMoney > 0 ){
     const donation = gettextFieldValueById('main-balance');
     const noakhaliTotalBalance = gettextFieldValueById('total-noakhali-balance');
     console.log(donation, donateMoney, noakhaliTotalBalance);
@@ -16,9 +16,29 @@ if(donateMoney > 0){
     document.getElementById('total-noakhali-balance').innerText = newNoakhaliBalance;
 }
 else {
-    alert('Faild to donate money'); 
+    alert('Faild to donate money..please try again'); 
 }
-})
+
+ // history
+
+ const historyItem = document.createElement("div");
+ historyItem.className = 'bg-white p-3 rounded-lg border-2 border-gray-500 p-10 ';
+
+ historyItem.innerHTML = `
+     <h3 class="text-xl font-bold text-black">${inputDonateNoakhali} Donate for Flood at Noakhali, Bangladesh</h3>
+     <p>Date: ${new Date().toString()}</p>
+ `;
+
+ const historyContainer = document.getElementById("history-list");
+ historyContainer.insertBefore(historyItem, historyContainer.firstChild);
+
+
+
+ // !Modal
+ document.getElementById("congratulation-modal").style.display = "flex";
+ document.getElementById("modal-custom-text").innerText = "Noakhali Flood"
+
+});
 
 
 // for Feni
@@ -39,7 +59,7 @@ if(donateMoney > 0){
     document.getElementById('total-feni-balance').innerText = newfeniBalance;
 }
 else {
-    alert('Faild to donate money'); 
+    alert('Faild to donate money..please try again'); 
 }
 })
 
@@ -61,6 +81,6 @@ if(donateMoney > 0){
     document.getElementById('total-quota-balance').innerText = newquotaBalance;
 }
 else {
-    alert('Faild to donate money'); 
+    alert('Faild to donate money..please try again'); 
 }
 })
